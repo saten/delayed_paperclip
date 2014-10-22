2ba440c75574516dc10b27700f4ceb40143d9da9
Sante Rotondi <saten.r@gmail.com>
2014-10-22 12:16:10 +0200

    wip

--- lib/delayed_paperclip.rb
+++ lib/delayed_paperclip.rb
@@ -77,25 +77,26 @@ module DelayedPaperclip
 
   module InstanceMethods
 
-    # setting each inididual NAME_processing to true, skipping the ActiveModel dirty setter
-    # Then immediately push the state to the database
-    def mark_enqueue_delayed_processing
-      unless @_enqued_for_processing_with_processing.blank? # catches nil and empy arrays
-        updates = @_enqued_for_processing_with_processing.collect{|n| "#{n}_processing = :true" }.join(", ")
-        updates = ActiveRecord::Base.send(:sanitize_sql_array, [updates, {:true => true}])
-        self.class.update_all(updates, "id = #{self.id}")
-      end
-    end
-
     # First mark processing
-    # then create
+    # then enqueue
     def enqueue_delayed_processing
       mark_enqueue_delayed_processing
+
       (@_enqued_for_processing || []).each do |name|
         enqueue_post_processing_for(name)
       end
       @_enqued_for_processing_with_processing = []
       @_enqued_for_processing = []
+    end
+
+    # setting each inididual NAME_processing to true, skipping the ActiveModel dirty setter
+    # Then immediately push the state to the database
+    def mark_enqueue_delayed_processing
+      unless @_enqued_for_processing_with_processing.blank? # catches nil and empty arrays
+        updates = @_enqued_for_processing_with_processing.collect{|n| "#{n}_processing = :true" }.join(", ")
+        updates = ActiveRecord::Base.send(:sanitize_sql_array, [updates, {:true => true}])
+        self.class.where(:id => self.id).update_all(updates)
+      end
     end
 
     def enqueue_post_processing_for name
@@ -108,6 +109,7 @@ module DelayedPaperclip
         @_enqued_for_processing_with_processing ||= []
         @_enqued_for_processing_with_processing << name
       end
+
       @_enqued_for_processing ||= []
       @_enqued_for_processing << name
     end
